1d0ed4568db325645bbc7b4338cb4a0ca0a21234
Mario Roberto <mrgomez.dev@gmail.com>
2024-03-22 15:30:33 -0600

    Go Navigation Top Bar

---
+++ app/src/main/java/com/example/navigationbar/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationbar.ui.theme.NavigationBarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationBarTheme {
+                val navController = rememberNavController()
+                val navigateAction = remember(navController) {
+                    MyAppNavigationActions(navController)
+                }
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val selectedDestination = navBackStackEntry?.destination?.route ?: MyAppRoute.HOME
+
+                MyAppContent(
+                    navController = navController,
+                    selectedDestination = selectedDestination,
+                    navigateTopLevelDestination = navigateAction::navigateTo
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MyAppContent(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    selectedDestination: String,
+    navigateTopLevelDestination: (MyAppTopLevelDestination) -> Unit
+) {
+    Row(modifier = modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            NavHost(
+                modifier = Modifier.weight(1f),
+                navController = navController,
+                startDestination = MyAppRoute.HOME
+            ) {
+                composable(MyAppRoute.HOME) {
+                    HomeScreen()
+                }
+                composable(MyAppRoute.ACCOUNT) {
+                    AccountScreen()
+                }
+                composable(MyAppRoute.SETTINGS) {
+                    SettingsScreen()
+                }
+            }
+            MyAppBottomNavigation(
+                selectedDestination = selectedDestination,
+                navigateTopLevelDestination = navigateTopLevelDestination
+            )
+        }
+    }
+}
+
+@Composable
+fun MyAppBottomNavigation(
+    selectedDestination: String,
+    navigateTopLevelDestination: (MyAppTopLevelDestination) -> Unit
+) {
+    NavigationBar(modifier = Modifier.fillMaxWidth()) {
+        TOP_LEVEL_DESTINATIONS.forEach { destinations ->
+            NavigationBarItem(
+                selected = selectedDestination == destinations.route,
+                onClick = { navigateTopLevelDestination(destinations) },
+                icon = {
+                    Icon(
+                        imageVector = destinations.selectedIcon,
+                        contentDescription = stringResource(id = destinations.iconTextId)
+                    )
+                }
+            )
+        }
+    }
+}
